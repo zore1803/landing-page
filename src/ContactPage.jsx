0b31610ad21d21322dab73aca73a447bad565cd8
Abhishek Jadhav <abhishekjadhav0078@gmail.com>
2026-07-07 16:52:43 +0530

    fix: Align logo across navigation and increase logo size

--- src/ContactPage.jsx
+++ src/ContactPage.jsx
@@ -97,27 +97,39 @@ function ContactPage() {
 
               <div className="git-divider" />
 
-              <div className="git-info-row">
-                <div className="git-info-icon">
-                  <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
-                    <path d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" strokeLinecap="round" strokeLinejoin="round"/>
-                    <path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
+              <div className="git-contact-group">
+                <div className="git-info-row">
+                  <div className="git-info-icon">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                      <path d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" strokeLinecap="round" strokeLinejoin="round"/>
+                      <path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                  </div>
+                  <p className="git-address">No. 721, Floor 7th, Centura Square IT Park, Rd Number 27, Wagle Industrial Estate, Thane West, Thane, Maharashtra 400604</p>
                 </div>
-                <p className="git-address">No. 721, Floor 7th, Centura Square IT Park, Rd Number 27, Wagle Industrial Estate, Thane West, Thane, Maharashtra 400604</p>
-              </div>
 
-              <div className="git-info-row">
-                <div className="git-info-icon git-info-icon--lg">
-                  <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
-                    <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" strokeLinecap="round" strokeLinejoin="round"/>
-                  </svg>
+                <div className="git-info-row">
+                  <div className="git-info-icon">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                      <path d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                  </div>
+                  <div className="git-contact-links">
+                    <a className="git-phone" href="tel:+919820933877">+91 98209 33877</a>
+                  </div>
                 </div>
-                <div className="git-contact-links">
-                  <a className="git-phone" href="tel:+919820933877">+91 98209 33877</a>
-                  <a className="git-email" href="mailto:sales@thecopperstudio.com">sales@thecopperstudio.com</a>
-                  <a className="git-email" href="mailto:support@thecopperstudio.com">support@thecopperstudio.com</a>
-                  <a className="git-email" href="mailto:help@thecopperstudio.com">help@thecopperstudio.com</a>
+
+                <div className="git-info-row">
+                  <div className="git-info-icon">
+                    <svg fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                    </svg>
+                  </div>
+                  <div className="git-contact-links">
+                    <a className="git-email" href="mailto:sales@thecopperstudio.com">sales@thecopperstudio.com</a>
+                    <a className="git-email" href="mailto:support@thecopperstudio.com">support@thecopperstudio.com</a>
+                    <a className="git-email" href="mailto:help@thecopperstudio.com">help@thecopperstudio.com</a>
+                  </div>
                 </div>
               </div>
 
